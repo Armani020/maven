7d4bfc5ac3bb747e52a2a6c234836c93defcc659
Armani020 <ar.amirgaliev.2015@gmail.com>
2022-06-27 18:46:53 +0600

    second commit

--- src/main/java/kz/halykacademy/App.java
+++ src/main/java/kz/halykacademy/App.java
@@ -1,13 +1,41 @@
 package kz.halykacademy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter the name: ");
+
+        String name = sc.nextLine();
+
+        var values = new HashMap<String, String>() {{
+            put("name", name);
+            put("salary", "123");
+            put("age", "18");
+        }};
+
+        var objectMapper = new ObjectMapper();
+        String requestBody = objectMapper.writeValueAsString(values);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("https://httpbin.org/post"))
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
     }
 }
